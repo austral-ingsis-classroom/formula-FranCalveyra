07873f47321c8340127e35dde76c4fe2abd53a85
FranC-prog <franciscocalveyra24@gmail.com>
2024-05-26 19:59:58 -0300

    Added visitor tests

--- src/test/java/edu/austral/ingsis/math/VisitorTest.java
+++ src/test/java/edu/austral/ingsis/math/VisitorTest.java
@@ -16,90 +16,129 @@ public class VisitorTest {
 
   @Test
   public void additionVisitTest() {
+    final String expected = "(1 + 1)";
     final Printer printer = new Printer();
     final Addition addition =
         new Addition(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
     printer.visit(addition);
-    assertEquals("(1 + 1)", printer.getLastVisitedValue());
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    addition.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void subtractionVisitTest() {
+    final String expected = "(1 - 1)";
     final Printer printer = new Printer();
-    final Subtraction addition =
+    final Subtraction subtraction =
         new Subtraction(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
-    printer.visit(addition);
-    assertEquals("(1 - 1)", printer.getLastVisitedValue());
+    printer.visit(subtraction);
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    subtraction.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void divisionVisitTest() {
+    final String expected = "(1 / 1)";
     final Printer printer = new Printer();
-    final Division addition =
+    final Division division =
         new Division(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
-    printer.visit(addition);
-    assertEquals("(1 / 1)", printer.getLastVisitedValue());
+    printer.visit(division);
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    division.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void productVisitTest() {
+    final String expected = "(1 * 1)";
     final Printer printer = new Printer();
-    final Product addition =
+    final Product product =
         new Product(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
-    printer.visit(addition);
-    assertEquals("(1 * 1)", printer.getLastVisitedValue());
+    printer.visit(product);
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    product.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void powerVisitTest() {
+    final String expected = "(1 ^ 1)";
     final Printer printer = new Printer();
-    final Power addition =
-        new Power(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
-    printer.visit(addition);
-    assertEquals("(1 ^ 1)", printer.getLastVisitedValue());
+    final Power power = new Power(new Constant(new MathNumber(1)), new Constant(new MathNumber(1)));
+    printer.visit(power);
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    power.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void squareRootVisitTest() {
+    final String expected = "1 ^ (1/2)";
     final Printer printer = new Printer();
-    final SquareRoot addition = new SquareRoot(new Constant(new MathNumber(1)));
-    printer.visit(addition);
-    assertEquals("1 ^ (1/2)", printer.getLastVisitedValue());
+    final SquareRoot sqrt = new SquareRoot(new Constant(new MathNumber(1)));
+    printer.visit(sqrt);
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    sqrt.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void constantVisitTest() {
+    final String expected = "1";
     final Printer printer = new Printer();
     final Constant c = new Constant(new MathNumber(1));
     printer.visit(c);
-    assertEquals("1", printer.getLastVisitedValue());
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    c.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void variableVisitTest() {
+    final String expected = "x";
     final Printer printer = new Printer();
     final Variable x = new Variable("x");
     printer.visit(x);
-    assertEquals("x", printer.getLastVisitedValue());
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    x.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void variableFunctionVisitTest() {
+    final String expected = "x + 1";
     final Printer printer = new Printer();
     final Variable x = new Variable("x");
     final VariableFunction function =
         new VariableFunction(new Addition(x, new Constant(new MathNumber(1))));
     printer.visit(function);
-    assertEquals("x + 1", printer.getLastVisitedValue());
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    function.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 
   @Test
   public void nonVariableFunctionVisitTest() {
+    final String expected = "3 + 1";
     final Printer printer = new Printer();
     final NonVariableFunction function =
         new NonVariableFunction(
             new Addition(new Constant(new MathNumber(3)), new Constant(new MathNumber(1))));
     printer.visit(function);
-    assertEquals("3 + 1", printer.getLastVisitedValue());
+    assertEquals(expected, printer.getLastVisitedValue());
+    Printer otherPrinter = new Printer();
+    function.accept(otherPrinter);
+    assertEquals(expected, otherPrinter.getLastVisitedValue());
   }
 }
